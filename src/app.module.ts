84282b7fe8abae61faef9386b593528115079a2d
Jawadi Rami <67319552+Ramijawadi@users.noreply.github.com>
2024-10-11 17:35:13 +0100

    Add user and video schema and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,35 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { JwtModule } from '@nestjs/jwt';
+import { secret } from './utils/constants';
+import { join } from 'path/posix';
+
+
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
-  ],
+  // imports: [MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+  imports:[MongooseModule.forRoot('mongodb+srv://ramijawadi104:1234@cluster0.rwhv9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'), 
+
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+
+   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+// mongodb+srv://ramijawadi104:<db_password>@cluster0.rwhv9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
